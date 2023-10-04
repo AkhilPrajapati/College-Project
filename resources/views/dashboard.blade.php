f19f083f611a5a5614488ef991f6d8de0247eca4
Akhil Prajapati <ak9863197745@gmail.com>
2023-10-04 14:37:43 +0545

    PC to Git

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,28 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+    <h1>Welcome</h1>
+
+    <a style="color: blue" href="/student">student</a>
+    <a style="color: blue" href="/student">student</a>
+    <a style="color: blue" href="/student">student</a>
+
+    <br>
+
+
+    {{-- <a href="">student</a> --}}
+    {{-- <a href="">student</a> --}}
+
+
+    {{-- <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <x-jet-welcome />
+            </div>
+        </div>
+    </div> --}}
+</x-app-layout>
+
